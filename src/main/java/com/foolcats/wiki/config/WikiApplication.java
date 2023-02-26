8a5d6b076ae453fc25079a7cfd75521fbf6c1edc
fool-cats <3331335256@qq.com>
2023-02-26 13:56:08 +0800

    2-6 hello world interface

--- src/main/java/com/foolcats/wiki/config/WikiApplication.java
+++ src/main/java/com/foolcats/wiki/config/WikiApplication.java
@@ -1,14 +1,16 @@
-package com.foolcats.wiki;
+package com.foolcats.wiki.config;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.env.Environment;
 
 //import java.util.logging.Logger;
 
 @SpringBootApplication
+@ComponentScan("com.foolcats")
 public class WikiApplication {
 
     private static final Logger LOG = LoggerFactory.getLogger(WikiApplication.class);
