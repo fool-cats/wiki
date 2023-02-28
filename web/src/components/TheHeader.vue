8ffbb82fc1ff8549295b45e6ef3fb703c7674e1c
fool-cats <3331335256@qq.com>
2023-02-28 20:04:31 +0800

    6-2 add router to header menu

--- web/src/components/TheHeader.vue
+++ web/src/components/TheHeader.vue
@@ -7,9 +7,15 @@
                 mode="horizontal"
                 :style="{ lineHeight: '64px' }"
             >
-                <a-menu-item key="1">nav 1</a-menu-item>
-                <a-menu-item key="2">nav 2</a-menu-item>
-                <a-menu-item key="3">nav 3</a-menu-item>
+                <a-menu-item key="1">
+                    <router-link to="/">首页</router-link>
+                </a-menu-item>
+                <a-menu-item key="2">
+                    <router-link to="/admin/ebook">管理</router-link>
+                </a-menu-item>
+                <a-menu-item key="3">
+                    <router-link to="/about">关于</router-link>
+                </a-menu-item>
             </a-menu>
         </a-layout-header>
 </template>
