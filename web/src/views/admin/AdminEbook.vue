1e2edfa2fd495cd370ad15bace2abb83cd092c7d
fool-cats <3331335256@qq.com>
2023-02-28 20:13:07 +0800

    6-3 change admin page layout

--- web/src/views/admin/AdminEbook.vue
+++ web/src/views/admin/AdminEbook.vue
@@ -1,5 +1,16 @@
 <template>
-    <div>
-        admin page
-    </div>
+    <a-layout>
+        <a-layout-content
+            :style="{
+                background: '#fff',
+                padding: '24px',
+                margin: 0,
+                minHeight: '280px',
+            }"
+        >
+            <div>
+                <h1>admin page</h1>
+            </div>
+        </a-layout-content>
+    </a-layout>
 </template>
