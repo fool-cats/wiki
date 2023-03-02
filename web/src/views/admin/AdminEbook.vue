2a04bc75fcd087d7013bf33b0b5c9a30a7e39956
fool-cats <3331335256@qq.com>
2023-03-02 15:20:05 +0800

    6-12 增加按电子书名字查询功能。

--- web/src/views/admin/AdminEbook.vue
+++ web/src/views/admin/AdminEbook.vue
@@ -9,9 +9,30 @@
             }"
         >
             <p>
-                <a-button type="primary" @click="add()" size="large">
-                    新增
-                </a-button>
+                <a-form layout="inline" :model="param">
+                    <a-form-item>
+                        <a-input v-model:value="param.name" placeholder="名称">
+                        </a-input>
+                    </a-form-item>
+                    <a-form-item>
+                        <a-button
+                            type="primary"
+                            @click="
+                                handleQuery({
+                                    page: 1,
+                                    size: pagination.pageSize,
+                                })
+                            "
+                        >
+                            查询
+                        </a-button>
+                    </a-form-item>
+                    <a-form-item>
+                        <a-button type="primary" @click="add()" size="large">
+                            新增
+                        </a-button>
+                    </a-form-item>
+                </a-form>
             </p>
             <a-table
                 :columns="columns"
@@ -94,13 +115,13 @@
 <script lang="ts">
 import { defineComponent, onMounted, ref } from "vue";
 import axios from "axios";
-import {message} from "ant-design-vue";
+import { message } from "ant-design-vue";
 
 export default defineComponent({
     name: "AdminEbook",
     setup() {
-        // const param = ref();
-        // param.value = {};
+        const param = ref();
+        param.value = {};
         const ebooks = ref();
         const pagination = ref({
             current: 1,
@@ -153,7 +174,8 @@ export default defineComponent({
                 .get("/ebook/list", {
                     params: {
                         page: params.page,
-                        size: params.size,
+                        size: params.size,  
+                        name: param.value.name,
                     },
                 })
                 .then((response) => {
@@ -167,7 +189,7 @@ export default defineComponent({
                         pagination.value.current = params.page;
                         // pagination.value.total = data.content.total;
                         pagination.value.total = data.content.total;
-                    }else {
+                    } else {
                         message.error(data.message);
                     }
                 });
@@ -190,15 +212,15 @@ export default defineComponent({
                 modalLoading.value = false;
                 if (data.success) {
                     modalVisible.value = false;
-                    
+
                     // 重新加载列表
                     handleQuery({
                         page: pagination.value.current,
                         size: pagination.value.pageSize,
                     });
-                }else {
+                } else {
                     message.error(data.message);
-                  }
+                }
             });
 
             // setTimeout(() => {
@@ -269,7 +291,7 @@ export default defineComponent({
         });
 
         return {
-            // param,
+            param,
             // message,
             ebooks,
             pagination,
@@ -283,6 +305,7 @@ export default defineComponent({
 
             ebook,
             modalLoading,
+            handleQuery,
             modalVisible,
             handleModalOk,
         };
