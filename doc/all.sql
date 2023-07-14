7fde8e3d6251561148e752973d8a676322d0e6f8
fool-cats <3331335256@qq.com>
2023-07-14 20:02:04 +0800

    8-8 文档内容建表

--- doc/all.sql
+++ doc/all.sql
@@ -100,3 +100,28 @@ INSERT INTO `doc` VALUES (6, 1, 5, '文章3.1.1', 1, 19, 1, 0, '2021-09-15 21:01
 INSERT INTO `doc` VALUES (1437317930831314945, 1, 0, '文章3', 3, 8, 1, 0, '2021-09-15 21:01:21', '2021-09-15 21:01:21');
 INSERT INTO `doc` VALUES (1438000187241934850, 2, 0, 'Ant Design of Vue', 1, 90, 6, 0, '2021-09-15 21:01:21', '2021-09-15 21:01:21');
 INSERT INTO `doc` VALUES (1438000644823724034, 2, 1438000187241934850, '关于ant-design-vue', 3, 50, 4, 0, '2021-09-15 21:01:21', '2021-10-06 00:25:00');
+
+
+
+
+-- ----------------------------
+-- Table structure for content
+-- ----------------------------
+DROP TABLE IF EXISTS `content`;
+CREATE TABLE `content`  (
+                            `id` bigint NOT NULL COMMENT '文章id',
+                            `ebook_id` bigint NULL DEFAULT NULL COMMENT '专栏id',
+                            `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '标题',
+                            `content` mediumtext CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '内容',
+                            `deleted` int NOT NULL DEFAULT 0 COMMENT '逻辑删除',
+                            PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '文章内容' ROW_FORMAT = DYNAMIC;
+
+-- ----------------------------
+-- Records of content
+-- ----------------------------
+INSERT INTO `content` VALUES (1, 1, '文章', '<p>12312678</p>', 0);
+INSERT INTO `content` VALUES (2, 1, '文章1.1', '<p>123456</p>', 0);
+INSERT INTO `content` VALUES (4, 1, '文章2.1.2', '', 0);
+INSERT INTO `content` VALUES (1438000187241934850, 2, 'Ant Design of Vue', '<blockquote><h1><font size=\"5\">这里是 Ant Design 的 Vue 实现，开发和服务于企业级后台产品。</font></h1></blockquote><p><br/></p><p><img src=\"https://gw.alipayobjects.com/zos/rmsportal/KDpgvguMpGfqaHPjicRK.svg\" width=\"167.43\" height=\"167.43\"/>&nbsp; &nbsp;&nbsp;<font size=\"5\">+&nbsp; &nbsp;&nbsp;</font><img src=\"https://qn.antdv.com/vue.png\" width=\"158.43\" height=\"158.43\"/><br/></p><p>特性</p><ul><li>提炼自企业级中后台产品的交互语言和视觉风格。</li><li>开箱即用的高质量 Vue 组件。</li><li>共享<a href=\"http://ant-design.gitee.io/docs/spec/introduce-cn\" target=\"_blank\">Ant Design of React</a>设计工具体系。</li></ul>', 0);
+INSERT INTO `content` VALUES (1438000644823724034, 2, '关于ant-design-vue', '<p>众所周知，Ant Design 作为一门设计语言面世，经历过多年的迭代和积累，它对 UI 的设计思想已经成为一套事实标准，受到众多前端开发者及企业的追捧和喜爱，也是 React 开发者手中的神兵利器。希望 ant-design-vue 能够让 Vue 开发者也享受到 Ant Design 的优秀设计。</p><p>ant-design-vue 是 Ant Design 的 Vue 实现，组件的风格与 Ant Design 保持同步，组件的 html 结构和 css 样式也保持一致，真正做到了样式 0 修改，组件 API 也尽量保持了一致。</p><p>Ant Design Vue 致力于提供给程序员愉悦的开发体验!</p>', 0);
---
+++ src/main/java/com/foolcats/wiki/domain/Content.java
@@ -0,0 +1,68 @@
+package com.foolcats.wiki.domain;
+
+public class Content {
+    private Long id;
+
+    private Long ebookId;
+
+    private String name;
+
+    private Integer deleted;
+
+    private String content;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getEbookId() {
+        return ebookId;
+    }
+
+    public void setEbookId(Long ebookId) {
+        this.ebookId = ebookId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getDeleted() {
+        return deleted;
+    }
+
+    public void setDeleted(Integer deleted) {
+        this.deleted = deleted;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(getClass().getSimpleName());
+        sb.append(" [");
+        sb.append("Hash = ").append(hashCode());
+        sb.append(", id=").append(id);
+        sb.append(", ebookId=").append(ebookId);
+        sb.append(", name=").append(name);
+        sb.append(", deleted=").append(deleted);
+        sb.append(", content=").append(content);
+        sb.append("]");
+        return sb.toString();
+    }
+}
